f57ff22fea84fdf284cbb39d9955e1165958b682
benjamin99 <cknomber1@gmail.com>
2017-01-14 20:55:50 +0800

    first test project

---
+++ simplest-test/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: `${__dirname}/app/index.html`,
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: [
+    './app/index.js',
+  ],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index_bundle.js',
+  },
+  module: {
+    // loaders 則是放欲使用的 loaders，在這邊是使用 babel-loader 將所有 .js（這邊用到正則式）相關檔案（排除了 npm 安裝的套件位置 node_modules）轉譯成瀏覽器可以閱讀的 JavaScript。
+    // preset 則是使用的 babel 轉譯規則，這邊使用 react、es2015。若是已經單獨使用 .babelrc 作為 presets 設定的話，則可以省略 query
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+  devServer: {
+    inline: true,
+    port: 8080,
+  },
+  plugins: [HTMLWebpackPluginConfig],
+};
